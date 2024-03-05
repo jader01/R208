c0d2da8d1202e383d9594382aa12f08bd253a091
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-03-05 11:46:31 +0100

    Add files via upload

---
+++ R208.py
@@ -0,0 +1,36 @@
+##################################################################################
+#                                 1.   ajout d'une valeur 
+################################################################################
+
+
+def adval():
+    #print("souhaiter vous ajouter une info ?")
+
+    #variable date
+    jours=('3')
+    mois='mars'
+    annee='2004'
+    date=(jours, mois, annee)
+    print(date)
+
+    #variable heure
+    h=18
+    minu=15
+    sec=1
+    heure=(h, minu, sec)
+    print(heure)
+
+    #identifiant du capteur
+    id
+
+    #tableau de stockage des valeurs
+    tab=[date, heure, IDcap, val, data]
+
+    #return(tab)
+
+
+##################################################################################
+#                                    apl de fontion
+################################################################################
+
+adval()
