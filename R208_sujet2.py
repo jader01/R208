e22db42979409fca8c9161bc68c1f49b4ccc4c61
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-03-15 14:33:41 +0100

    Update R208_sujet2.py

--- R208_sujet2.py
+++ R208_sujet2.py
@@ -2,19 +2,23 @@
 #                          creation des fonction
 #############################################################################################
 
+
+#1. creation des deux tableau
 def adperson(tab, nom, prenom, sexe, date):
     tab.append([nom, prenom, sexe, date]) #on rajouter toutes ces valeur dans un tableau
-    print(tab)
+    #print(tab)
     return(tab)
     
-
-
-
-def adparent(enfant, parent):
+def adparent(tab, enfant, parent):
     tab.append([enfant, parent])
     print(tab)
     return(tab)
 
+
+#2. affichage du tableau
+def printtab():
+    for person in tab :
+        print(tab)
 
 
 
@@ -28,3 +32,10 @@ def adparent(enfant, parent):
 
 tab=[]
 adperson(tab, 'rueda lucantis', 'jade', 2, (2004,3,3))
+adperson(tab, 'jean', 'coline', 2, (1971,7,12))
+adperson(tab, 'dupouy', 'bernard', 1, (2007,6,14))
+
+printtab()
+
+#adparent(tab, 0, 1)
+
