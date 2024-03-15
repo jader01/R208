c76ead090ccdf366d520afea5aa1f31afbd950a2
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-03-15 14:10:07 +0100

    Add files via upload

---
+++ sujet2.py
@@ -0,0 +1,30 @@
+#############################################################################################
+#                          creation des fonction
+#############################################################################################
+
+def adperson(tab, nom, prenom, sexe, date):
+    tab.append([nom, prenom, sexe, date]) #on rajouter toutes ces valeur dans un tableau
+    print(tab)
+    return(tab)
+    
+
+
+
+def adparent(enfant, parent):
+    tab.append([enfant, parent])
+    print(tab)
+    return(tab)
+
+
+
+
+
+
+
+    
+##################################################################
+#                           apl de fonction
+#################################################################
+
+tab=[]
+adperson(tab, 'rueda lucantis', 'jade', 2, (2004,3,3))
